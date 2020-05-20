b9a87c0ebbe2c4fa7d020fa8789473f10b2e9eb2
Dreammer12138 <wangshibo142@gmail.com>
2020-05-20 21:17:48 +0800

    Fixed

--- begin.sh
+++ begin.sh
@@ -1,8 +1,3 @@
 #!/usr/bash
-wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-sudo chmod a+x ./install-release.sh
-sudo bash ./install-release.sh
-sudo systemctl start v2ray
-
 sudo python3 manage.py migrate
 sudo pip3 install -r requirements.txt
